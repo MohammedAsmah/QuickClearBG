5c4450c73a02612191f723c610e770529c34f7e8
Simo Asmah <simoasmah@gmail.com>
2025-02-06 19:40:59 +0100

    fuxing the webhooks

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,55 +1,46 @@
-import { Webhook } from "svix"
-import userModel from "../models/userModel.js"
-
-//api controller function to manage clerk user with database
-const clerkWebhooks=async (req,res)=>{
+const clerkWebhooks = async (req, res) => {
     try {
-        //create svix anstance wth clerk webhook secret
-        const Whook =new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-        await Whook.verify(JSON.stringify(req.body),{
-            "svix-id":req.headers["svix-id"],
-            "svix-timestamp":req.headers["svix-timestamp"],
-            "svix-signature":req.headers["svix-signature"],
-        })
-
-        const{data,type}=req.body
-       switch (type) {
-        case "user.created":{
-            const userData={
-                clerkId:data.id,
-                email:data.email_addresses[0].email_address,
-                firstName:data.first_name,
-                lastName:data.last_name,
-                photo:data.image_url
-            }
-            await userModel.create(userData)
-            res.json({})
-            break;
-        }
-        case "user.updated":{
-            const userData={
-                email:data.email_addresses[0].email_address,
-                firstName:data.first_name,
-                lastName:data.last_name,
-                photo:data.image_url
-            }
-            await userModel.findOneAndUpdate({clerkId:data.id},userData)
-            res.json({})
-            break;
-        }
-        case "user.deleted":{
-            await userModel.findOneAndDelete({clerkId:data.id})
-            res.json({})
-            break;
-        }
-        default:
-            break;
-       }
-    } catch (error) {
-        console.log(error.messsage);
-        res.json({success:false,message:error.message})
+        const Whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
         
-    }
+        await Whook.verify(JSON.stringify(req.body), {
+            'svix-id': req.headers["svix-id"],
+            'svix-timestamp': req.headers["svix-timestamp"],
+            'svix-signature': req.headers["svix-signature"],
+        });
 
-}
-export {clerkWebhooks}
+        const { data, type } = req.body;
+
+        switch (type) {
+            case "user.created": {
+                const userData = {
+                    clerkId: data?.id,
+                    email: data?.email_addresses?.[0]?.email_address,
+                    firstName: data?.first_name,
+                    lastName: data?.last_name,
+                    photo: data?.image_url
+                };
+                await userModel.create(userData);
+                return res.status(200).json({ success: true });
+            }
+            case "user.updated": {
+                const userData = {
+                    email: data?.email_addresses?.[0]?.email_address,
+                    firstName: data?.first_name,
+                    lastName: data?.last_name,
+                    photo: data?.image_url
+                };
+                await userModel.findOneAndUpdate({ clerkId: data.id }, userData);
+                return res.status(200).json({ success: true });
+            }
+            case "user.deleted": {
+                await userModel.findOneAndDelete({ clerkId: data.id });
+                return res.status(200).json({ success: true });
+            }
+            default:
+                return res.status(400).json({ success: false, message: 'Unknown webhook type' });
+        }
+    } catch (error) {
+        console.error('Webhook error:', error);
+        return res.status(500).json({ success: false, message: error.message });
+    }
+};
